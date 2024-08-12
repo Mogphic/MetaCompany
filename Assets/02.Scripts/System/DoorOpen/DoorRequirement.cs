02b750dca9f803054a0d7018d8fced7d11ecaf83
lofell <lofell@naver.com>
2024-08-12 13:25:59 +0900

    도어 오픈 조건 만들기

---
+++ Assets/02.Scripts/System/DoorOpen/DoorRequirement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DoorRequirement : MonoBehaviour
+{
+    public bool isSuccess = false;
+
+    public GameObject prefabToCompare;
+    
+
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if (IsSamePrefabType(other.gameObject, prefabToCompare))
+        {
+            Debug.Log(other.name + "는 지정된 프리팹과 동일한 타입입니다.");
+            isSuccess = true;
+        }
+    }
+
+    private bool IsSamePrefabType(GameObject detectedObject, GameObject prefab)
+    {
+        // 두 오브젝트의 프리팹 인스턴스 ID를 가져옵니다.
+        Object detectedPrefab = PrefabUtility.GetCorrespondingObjectFromSource(detectedObject);
+        Object comparePrefab = PrefabUtility.GetCorrespondingObjectFromSource(prefab);
+
+        // 프리팹 인스턴스 ID가 같은지 비교합니다.
+        return detectedPrefab == comparePrefab && detectedPrefab != null;
+    }
+}
---
+++ Assets/02.Scripts/System/DoorOpen/InteractionKillDoor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class InteractionKillDoor : InteractionDoorDataBase
+{
+    public GameObject target;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        if (target == null)
+            Debug.LogError("Target없음");
+    }
+    public void Update()
+    {
+        if(target.IsDestroyed())
+        {
+            isOpened = true;
+        }
+    }
+}
