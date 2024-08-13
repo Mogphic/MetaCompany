d792db962bc413cca4f502da355b93e6d3c51db5
SongMinJe <dlsgur3543@gmail.com>
2024-08-14 02:30:51 +0900

    LoadScene_AnimationDelay_Fix

--- Assets/02.Scripts/Player/PlayerAnimation.cs
+++ Assets/02.Scripts/Player/PlayerAnimation.cs
@@ -6,7 +6,7 @@ public class PlayerAnimation : MonoBehaviour
 {
     private Animator animator;
     private bool isEndAtk = true;
-    private void Awake()
+    private void Start()
     {
         animator = GetComponent<Animator>();
     }
